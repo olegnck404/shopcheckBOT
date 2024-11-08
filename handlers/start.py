c342632246aa71eb011fd9dbcbc63a8ab9dbdee0
Avazart <avazart7@gmail.com>
2024-11-08 11:52:02 +0200

    Fix keyboards

--- handlers/start.py
+++ handlers/start.py
@@ -1,25 +1,13 @@
 from aiogram import types, Router
-from aiogram.filters import Command  # Import Command filter
+from aiogram.filters import CommandStart
+
+from keyboards.keyboards import create_main_keyboard
 
 router = Router(name=__name__)
 
-
+@router.message(CommandStart())
 async def start_command(message: types.Message):
-    await message.answer("Привет! Выберите действие:", reply_markup=create_main_keyboard())
+    await message.answer("Привет! Выберите действие:",
+                         reply_markup=create_main_keyboard())
 
-def create_main_keyboard():
-    # Create an instance of ReplyKeyboardMarkup
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    # Define buttons
-    button_search = types.KeyboardButton("🔍 Поиск по артикулу")
-    button_all_products = types.KeyboardButton("📋 Все товары")
-
-    # Add buttons to the keyboard
-    keyboard.add(button_search, button_all_products)
-
-    return keyboard
-
-def register_start_handler(router: Router):
-    # Register the command handler with the Command filter
-    router.message.register(start_command, Command(commands=['start']))
