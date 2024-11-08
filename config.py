994d3366692a26bc686e4d1ec8614a0670adf07e
Avazart <avazart7@gmail.com>
2024-11-08 11:05:20 +0200

    Hide secret data, move settings into '.env' file

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-API_TOKEN = "1706854733:AAH008bg4R7Y8PI6fSD8dKuUoPxsSLN9Tuw"
-ADMIN_PASSWORD = "158x12llo"
-DATABASE_NAME = "products.db"
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.environ['API_TOKEN']
+ADMIN_PASSWORD = os.environ['ADMIN_PASSWORD']
+DATABASE_NAME = os.environ['DATABASE_NAME']
